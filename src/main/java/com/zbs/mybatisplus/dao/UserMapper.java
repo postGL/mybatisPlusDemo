ecbd9fb2cb809f59cf3946b19bcd31a6d77a3668
zbs <zhangbaoshuai@cph.citic>
2020-10-16 09:23:01 +0800

    feat(mybatisPlus): @MapperScan和@Repository

--- src/main/java/com/zbs/mybatisplus/dao/UserMapper.java
+++ src/main/java/com/zbs/mybatisplus/dao/UserMapper.java
@@ -10,6 +10,12 @@ import org.springframework.stereotype.Repository;
  * author: zhangbs
  * version: 1.0
  */
+/**
+ * 配置了@MapperScan("com.zbs.mybatisplus.dao")包扫描和mapper上注解@Repository都会在spring中
+ * 注册bean，作用一样，
+ * 不一样的地方就是，前者在注入UserMapper时报红线找不到，但是可以用（编译阶段找不到，运行可以）
+ * 后者没有此问题
+ * */
 @Repository
 public interface UserMapper extends BaseMapper<User> {
 }
