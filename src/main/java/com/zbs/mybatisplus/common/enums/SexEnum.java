22fa7d450e34c507c441021b69aa5a6aee12e185
zbs <250468591@qq.com>
2023-02-07 10:13:08 +0800

    feat: mybatisPlus 枚举返回，如果数据库的数值，在枚举中找不到对应的值，则会导致不返回该字段

--- src/main/java/com/zbs/mybatisplus/common/enums/SexEnum.java
+++ src/main/java/com/zbs/mybatisplus/common/enums/SexEnum.java
@@ -20,8 +20,9 @@ public enum SexEnum {
      */
     MAN(1, "男");
 
-    @EnumValue
-    @JsonValue    //标记响应json值
+    // 如果数据库的数值，在枚举中找不到对应的值，则会导致不返回该字段
+    @EnumValue     //在需要保存到数据库的值上面加上注解
+    @JsonValue    //标记响应json值，需要在前端展示哪个值就在哪个属性上加上该注解
     private int sex;
 
     private String description;
